299e3a99845ddae1e0d3a7465a483c43fe08cd80
bigesmcfly <bigesmcfly@gmail.com>
2017-07-21 19:56:11 +0200

    Initial

---
+++ module1/src/base/Start.java
@@ -0,0 +1,16 @@
+package base;
+
+public class Start {
+
+    public Start() {
+        init();
+    }
+
+    public static void main(String[] args) {
+        new Start();
+    }
+
+    private void init() {
+        System.out.print("init");
+    }
+}
